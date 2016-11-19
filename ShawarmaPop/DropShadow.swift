96ea311d0611dba6e63aa84c842c0c42db14a346
Iulian David <david.iulian@gmail.com>
2016-11-19 09:39:20 +0200

    draft

---
+++ ShawarmaPop/DropShadow.swift
@@ -0,0 +1,17 @@
+//
+//  DropShadow.swift
+//  ShawarmaPop
+//
+//  Created by iulian david on 11/19/16.
+//  Copyright © 2016 iulian david. All rights reserved.
+//
+
+import UIKit
+
+protocol DropShadow {}
+
+//Constrain the Protocol to a certain Type
+//Just like with the generics
+extension DropShadow where Self: UIView {
+    
+}
